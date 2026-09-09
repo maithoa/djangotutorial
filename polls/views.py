420b9c518114aa85fabdc919be6b663c1750664a
maithoa <8200087+maithoa@users.noreply.github.com>
2026-09-09 20:53:45 +0300

    Feature: Update detail.htlm to allow vote submitting and views.py to update the vote in db

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.urls import reverse
+from django.db.models import F
 from .models import Question
 
 
@@ -17,5 +19,18 @@ def results(request, question_id):
     return HttpResponse(f"You're looking at the results of question {question_id}.")
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Question.choice_set.model.DoesNotExist):
+        return render(request, "polls/detail.html",{
+            "question": question, 
+            "error_message": "No choice selected."
+        },
+        )
+    else:
+        selected_choice.votes += F("votes")+1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+ 
 
