c943f883dbf320a080e74d11e622e2b891d12512
maithoa <8200087+maithoa@users.noreply.github.com>
2026-09-10 15:50:53 +0300

    Feature: Add Choices into Question Admin page and change layout of Question Admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,22 @@
+from dataclasses import fields
+
 from django.contrib import admin
 
 # Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
